3ea9f156711bcbc7f3130cf713de777177bd1d4c
f0go <f0go@protonmail.com>
2015-09-25 22:48:50 -0300

    Get all news and open detail

--- iOS/hackathonLN/DetailViewController.swift
+++ iOS/hackathonLN/DetailViewController.swift
@@ -12,6 +12,48 @@ import UIKit
 class DetailViewController: UIViewController {
     
     @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var holderHeight: NSLayoutConstraint!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var categoryWidth: NSLayoutConstraint!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var note: UILabel!
+    @IBOutlet weak var noteHeight: NSLayoutConstraint!
+    
+    var postID: Int!
+    
+    override func viewDidLoad() {
+        let url = NSURL(string: "http://contenidos.lanacion.com.ar/json/nota/\(postID)")
+        Utils.makeJsonRequest(url!, callback: { (json, error) -> Void in
+            if error != nil {
+                println(error)
+            }else {
+                let imageURL = "http://bucket.lanacion.com.ar" + json["imagenes"][0]["src"].stringValue
+                Utils.getImageFromUrl(imageURL, callback: { (image) -> Void in
+                    self.image.image = image
+                })
+                
+                self.titleLabel.text = json["titulo"]["valor"].stringValue
+                self.categoryLabel.text = json["categoria"]["valor"].stringValue
+                self.categoryLabel.sizeToFit()
+                self.categoryWidth.constant = self.categoryLabel.frame.width + 30
+                self.date.text = json["fecha"].stringValue
+                self.note.text = self.parseContent(json["contenido"])
+            }
+        }, retryCount: 0)
+    }
+    
+    func parseContent(JSON) -> String {
+        var parsedContent: String!
+        
+        
+        
+        
+        
+        
+        
+        return parsedContent
+    }
     
     @IBAction func backButton(sender: AnyObject) {
         self.navigationController?.popViewControllerAnimated(true)
--- iOS/hackathonLN/ViewController.swift
+++ iOS/hackathonLN/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
@@ -20,6 +21,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
         let vc = self.storyboard?.instantiateViewControllerWithIdentifier("DetailViewController") as! DetailViewController
+        vc.postID = lastNewsJSON[indexPath.row]["id"].intValue
         self.navigationController?.pushViewController(vc as DetailViewController, animated: true)
     }
     
@@ -29,6 +31,29 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("LastNewsCell", forIndexPath: indexPath) as! LastNewsCell
+
+        cell.categoryWidth.constant = 0
+        cell.categoryHeight.constant = 0
+        
+        cell.categoryLabel.text = lastNewsJSON[indexPath.row]["categoria"]["valor"].stringValue
+        cell.categoryLabel.sizeToFit()
+        cell.categoryWidth.constant = cell.categoryLabel.frame.width + 30
+        cell.categoryHeight.constant = 25
+        
+        cell.date.text = lastNewsJSON[indexPath.row]["fecha"].stringValue
+        
+        cell.title.text = lastNewsJSON[indexPath.row]["titulo"][0]["valor"].stringValue
+        
+        cell.backImage.image = nil
+        let imageURL = "http://bucket.lanacion.com.ar" + lastNewsJSON[indexPath.row]["imagenes"][0]["src"].stringValue
+        Utils.getImageFromUrl(imageURL, callback: { (image) -> Void in
+            dispatch_async(dispatch_get_main_queue(), {
+                if let cellToUpdate = collectionView.cellForItemAtIndexPath(indexPath) {
+                    let blur = image.applyBlurWithRadius(15, blurType: BOXFILTER, tintColor: UIColor.clearColor(), saturationDeltaFactor: 1, maskImage: nil)
+                    cell.backImage.image = blur
+                }
+            })
+        })
         
         return cell
     }
@@ -41,7 +66,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         ret = flowlayout.itemSize
         
         ret.width = Globals.screenSize.width
-        ret.height = Globals.screenSize.height
+        ret.height = Globals.screenSize.height + 10
             
         return ret
     }
